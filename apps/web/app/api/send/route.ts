133a3c0c38225b9ababa9977010e55bcf73323e5
laurent heneman <laurentheneman@mac-studio-de-laurent.home>
2024-04-05 11:29:30 +0200

    test: add a sended test email with edge function in production

---
+++ apps/web/app/api/send/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+export const runtime = "edge";
+export const dynamic = "force-dynamic";
+
+const RESEND_API_KEY = process.env.RESEND_API_KEY;
+
+export async function POST() {
+  const res = await fetch("https://api.resend.com/emails", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${RESEND_API_KEY}`,
+    },
+    body: JSON.stringify({
+      from: "laurent.heneman@edukeasy.com",
+      to: ["laurent.heneman@yahoo.fr"],
+      subject: "hello world",
+      html: "<strong>it works!</strong>",
+    }),
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    return NextResponse.json(data);
+  }
+}
--- apps/web/app/api/user/signup/route.ts
+++ apps/web/app/api/user/signup/route.ts
@@ -1,8 +1,8 @@
 import { Role } from "@prisma/client";
-// import bcrypt from "bcrypt";
-// import { randomBytes } from "crypto";
-// import { prisma } from "database";
-// import { generateConfirmationCode } from "lib/utils";
+import bcrypt from "bcrypt";
+import { randomBytes } from "crypto";
+import { prisma } from "database";
+import { generateConfirmationCode } from "lib/utils";
 import { NextResponse } from "next/server";
 
 import { sendEmail } from "@/emails";
@@ -14,50 +14,50 @@ interface IData {
 export async function POST(request: Request) {
   const { data } = (await request.json()) as IData;
 
-  //const defaultLocale = request.headers.get("x-default-locale") ?? "fr";
-  //const HASH = await bcrypt.hash(data.password, 10);
+  const defaultLocale = request.headers.get("x-default-locale") ?? "fr";
+  const HASH = await bcrypt.hash(data.password, 10);
 
   /** CREATE A NEW USER IN DB */
-  // const new_user = {
-  //   name: data.name,
-  //   email: data.email,
-  //   locale: defaultLocale,
-  //   image: "default",
-  //   password: HASH,
-  //   role: data.role || Role.TEACHER,
-  // };
+  const new_user = {
+    name: data.name,
+    email: data.email,
+    locale: defaultLocale,
+    image: "default",
+    password: HASH,
+    role: data.role || Role.TEACHER,
+  };
 
   /*   */
   try {
-    // const user = await prisma.user.create({
-    //   data: new_user,
-    // });
+    const user = await prisma.user.create({
+      data: new_user,
+    });
 
-    // await prisma.author.create({
-    //   data: {
-    //     name: data.name,
-    //     image: "default",
-    //     role: data.role || Role.TEACHER,
-    //     userId: user.id,
-    //   },
-    // });
+    await prisma.author.create({
+      data: {
+        name: data.name,
+        image: "default",
+        role: data.role || Role.TEACHER,
+        userId: user.id,
+      },
+    });
 
     /** CREATE A USER EMAIL CHECKED IN DB */
-    // const expiresAt = new Date();
-    // expiresAt.setDate(expiresAt.getDate() + 1);
-    // const token = randomBytes(32).toString("hex");
-    // const hashed = token;
-    // const code = generateConfirmationCode();
-    // await prisma.emailVerificationToken.create({
-    //   data: {
-    //     expiresAt,
-    //     token: hashed,
-    //     userId: user.id,
-    //     code: code,
-    //   },
-    // });
+    const expiresAt = new Date();
+    expiresAt.setDate(expiresAt.getDate() + 1);
+    const token = randomBytes(32).toString("hex");
+    const hashed = token;
+    const code = generateConfirmationCode();
+    await prisma.emailVerificationToken.create({
+      data: {
+        expiresAt,
+        token: hashed,
+        userId: user.id,
+        code: code,
+      },
+    });
 
-    //const url = `${process.env.VERCEL_URL}/validate?token=${token}&id=${user.id}`;
+    const url = `${process.env.VERCEL_URL}/validate?token=${token}&id=${user.id}`;
 
     await sendEmail({
       email: data.email,
@@ -65,18 +65,17 @@ export async function POST(request: Request) {
       react: WelcomeEmail({
         name: data.name,
         email: data.email,
-        code: "code",
-        url: "url",
+        code: code,
+        url: url,
       }),
       marketing: false,
       test: false,
     });
-    //const { password: _password, ...safeUser } = user;
+    const { password: _password, ...safeUser } = user;
 
     const json_response = {
       status: "success",
-      data : {}
-      //data: { safeUser },
+      data: { safeUser },
     };
 
     return new NextResponse(JSON.stringify(json_response), {
