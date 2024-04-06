1f16123d3d448e398b7c2f86070a5a6065a20b61
laurent heneman <laurentheneman@mac-studio-de-laurent.home>
2024-04-06 09:39:19 +0200

    ci: add highlight service for log session

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,10 +1,20 @@
 import "@/styles/globals.css";
-
+import { HighlightInit } from '@highlight-run/next/client'
 import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }: Readonly<React.PropsWithChildren>) {
   return (
     <>
+    <HighlightInit
+				projectId={'kgrw4wme'}
+				serviceName="my-nextjs-frontend"
+				tracingOrigins
+				networkRecording={{
+					enabled: true,
+					recordHeadersAndBody: true,
+					urlBlocklist: [],
+				}}
+			/>
       {children}
       <Analytics />
     </>
